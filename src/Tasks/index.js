0217ee8fc7db61824ee2717e52cfa49688ca624c
Angelika Stano <archiangel809@gmail.com>
2022-12-08 20:30:20 +0100

    js formatting

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,4 +1,3 @@
-import { getDefaultNormalizer } from "@testing-library/react";
 import React from "react";
 import "./style.css";
 
@@ -6,22 +5,20 @@ const Tasks = ({ tasks, hideDoneTasks }) => (
     <ul className="section__tasks">
         {tasks.map(task => (
             <li
-            className={`list__item ${
-                task.done && hideDoneTasks
-                ? "list__item--hidden"
-                : ""}`
-            }
-            key = {task.id}>
-            <button className="list__button list__button--done">
-              {task.done ? "✓" : ""}
-            </button>
-            <p className={`${
-                task.done ? "list__item--done" : ""}"`}>
-              {task.content}
-            </p>
-            <button className="list__button list__button--remove">
-              🗑
-            </button>
+                className={`list__item ${task.done && hideDoneTasks
+                        ? "list__item--hidden"
+                        : ""}`
+                }
+                key={task.id}>
+                <button className="list__button list__button--done">
+                    {task.done ? "✓" : ""}
+                </button>
+                <p className={`${task.done ? "list__item--done" : ""}"`}>
+                    {task.content}
+                </p>
+                <button className="list__button list__button--remove">
+                    🗑
+                </button>
             </li>
         ))}
     </ul>
